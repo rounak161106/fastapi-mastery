f0ff775433f73c89e9d7ec33a74afd6d34cb7995
Rounak Prasad <rounak16112006@gmail.com>
2026-04-28 16:29:11 +0530

    create hello world

--- Tutorials/hello-world.py
+++ Tutorials/hello-world.py
@@ -1,1 +1,19 @@
-from flask
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def hellO():
+    return {'msg' : 'Hello world'}
+
+@app.get('/about')
+def about():
+    return {"msg" : "This is about section"}
+
+@app.get('/user/{name}')
+def user_details(name):
+    return {"msg" : f"Hello {name}"}
+
+
+
+# to run this we have to write the command uvicorn <module_name>:app --reload  [for auto reload on any changes made to the code]
