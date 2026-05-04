270414afb375bad23d5d9b0d740ba1b21db89acc
Rounak Prasad <rounak16112006@gmail.com>
2026-05-04 17:49:39 +0530

    add list and dict validation

--- Tutorials/Pydantic/intro.py
+++ Tutorials/Pydantic/intro.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Annotated
 
 class Patient(BaseModel):
     name : str = Field(max_length=50)
-    age : Optional[int] = Field(default : None,gt=5, lt=80)
+    # age : Optional[int] = Field(default=None,gt=5, lt=80)
+    age : Annotated[str, Field(max_length=50, title="age of the patient", description="give the age", examples=[53,26])]
     linkedln : AnyUrl
     email : EmailStr
-    weight : float = Field(gt=0)
+    weight : Annotated[float, Field(gt=0, strict=True)] #with sstrict we can prevent the float value coming in string format
     married : bool
     allergies : List[str] = Field(max_length=5)   #here we could have used the list keyword but that would have only validated whether the allergies field is a list or not but here we could even check whether the contents within the list is of str type
     contact : Dict[str, str]
@@ -22,7 +23,7 @@ def insert_patient(patient : Patient):
     print(patient.linkedln)
     print("inserted successfully")
 
-patient_info = {"name" : "Rounak", 'weight' : 74.2, 'married' : True, 'allergies' : ['pollen', 'dust'], 'contact' : {'phone' : '7008961001'}, 'email' : "abc@gmail.com", 'linkedln' : 'http://linkedln.com/5135'}
+patient_info = {"name" : "Rounak", 'weight' : 74.2, 'age' : '52','married' : True, 'allergies' : ['pollen', 'dust'], 'contact' : {'phone' : '7008961001'}, 'email' : "abc@gmail.com", 'linkedln' : 'http://linkedln.com/5135'}
 patient_1 = Patient(**patient_info)
 
 insert_patient(patient_1)
