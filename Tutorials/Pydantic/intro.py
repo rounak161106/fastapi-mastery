bf69c5ed9a314c86864eb2b0d3339b979e77881e
Rounak Prasad <rounak16112006@gmail.com>
2026-05-04 17:06:05 +0530

    Add more validations

--- Tutorials/Pydantic/intro.py
+++ Tutorials/Pydantic/intro.py
@@ -1,8 +1,13 @@
 from pydantic import BaseModel
+from typing import List
 
 class Patient(BaseModel):
     name : str
     age : int
+    weight : float
+    married : bool
+    allergies : List[str]
+    contact : dict
 
 def insert_patient(patient : Patient):
     print(patient.name)
