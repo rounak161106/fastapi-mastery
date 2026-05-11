a61f8a6fbdce074f1217516a5641099f237217e3
Rounak Prasad <rounak16112006@gmail.com>
2026-05-11 23:27:28 +0530

    complete put

--- Tutorials/project/main.py
+++ Tutorials/project/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Path, HTTPException, Query #path is used to perform, validation, adding description to the api documentation(similarly the query)
 from pydantic import BaseModel, computed_field, Field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 from fastapi.responses import JSONResponse
 
@@ -31,6 +31,14 @@ class Patient(BaseModel):
             return "Overweight"
         else:
             return "Obesity"
+        
+class PatientUpdate(BaseModel):
+    name : Annotated[str, Field(default=None, description="Name of the patient", examples=['xyz'])]
+    city : Annotated[str, Field(default=None, description="City of the patient", examples=['Barbil'])]
+    age : Annotated[int, Field(default=None, description="Age of the patient", examples=[19], gt=0, lt=120)]
+    gender : Annotated[Literal['Male', 'Female', 'Others'], Field(default=None, description="Gender of the patient")]
+    height : Annotated[float, Field(default=None,gt=0, description="Height of the patient(in metres)", examples=[1.85])]
+    weight : Annotated[float, Field(default=None,gt=0, description="Weight of the patient(in kgs)", examples=[65.6])]
 
 def get_data():
     with open('patients_data.json', 'r') as f:
@@ -83,6 +91,23 @@ def create_patient(patient : Patient):
         raise HTTPException(status_code=400, detail="Patient already exists")
     
     #new patient add to database
-    data[patient.id] = patient.model_dump(  exclude=['id'])
+    data[patient.id] = patient.model_dump(exclude=['id'])
     save_data(data)
     return JSONResponse(status_code=201, content = {'message' : 'Patient created successfully'})        
+
+#updating patient(implementing put request)
+@app.put('/edit/{patient_id}')
+def update_patient(patient_update : PatientUpdate, patient_id : str = Path(..., description="Id of the patient", examples=['P001'])):
+    data = get_data()
+    if patient_id not in data:
+        raise HTTPException(status_code=400, detail="Patient not found")
+    this_patient_info = data[patient_id]
+    updated_patient_info = patient_update.model_dump(exclude_unset=True)
+    for key, value in updated_patient_info.items():
+        this_patient_info[key] = value
+
+    this_patient_info["id"] = patient_id
+    patient_pyd_obj = Patient(**this_patient_info)
+    this_patient_info = patient_pyd_obj.model_dump(exclude='id')
+    data[patient_id] = this_patient_info
+    save_data(data)
